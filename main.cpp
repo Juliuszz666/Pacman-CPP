90817d673ddad9d1bc0438e5d90137d9cce81246
Julian Bednarek <juliuszxxbednarek@gmail.com>
2024-08-27 11:10:12 +0200

    some gpt written stylesheet and whole layout for settings page ready

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFile file(":/stylesheet.qss");
+    if (file.open(QFile::ReadOnly)) {
+        QString styleSheet = file.readAll();
+        a.setStyleSheet(styleSheet);
+        file.close();
+    }
     MainWindow w;
     w.show();
     return a.exec();
